97a7ecb31a469d6dccd10108ea13e659f22ca1cb
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2020-07-09 08:58:05 +0100

    Refactor: refactored navigation components

--- src/components/homepage/Banner.js
+++ src/components/homepage/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Banner.scss'
+import Zoom from 'react-reveal/Zoom';
 
 const Banner = () => {
   return (
@@ -7,8 +8,10 @@ const Banner = () => {
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <h1 className="text-white">Gamerzz</h1>
-            <p className="text-grey">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus labore soluta molestias alias porro aliquam dicta ipsa recusandae commodi, maxime est pariatur corrupti unde voluptatem quaerat fugit distinctio quos magnam!</p>
+            <Zoom>
+              <h1 className="text-white">Gamerzz</h1>
+              <p className="text-grey">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus labore soluta molestias alias porro aliquam dicta ipsa recusandae commodi, maxime est pariatur corrupti unde voluptatem quaerat fugit distinctio quos magnam!</p>
+            </Zoom>
           </div>
           <div className="col-md-6">
 
