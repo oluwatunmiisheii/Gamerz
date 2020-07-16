f939faaf871cf01ab322c374d5d0e3fd5af14e3a
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2020-07-16 23:31:26 +0100

    feat: added spinner to developers route

---
+++ src/components/common/Preloader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const PreLoader = () => {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="spinner">
+            <div class="cube1"></div>
+            <div class="cube2"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PreLoader;
--- src/containers/developers/Developers.js
+++ src/containers/developers/Developers.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Cards from '../../components/common/Cards'
 import axios from '../../axios'
 import LoadMoreBtn from '../../components/common/LoadMoreBtn'
+import Preloader from '../../components/common/Preloader'
 // import TransitionGroup from 'react-transition-group/TransitionGroup';
 // import Fade from 'react-reveal/Fade';
 
@@ -9,7 +10,8 @@ class Developers extends Component {
   state = {
     pathName: '',
     developers: [],
-    paginatedDevelopers: []
+    paginatedDevelopers: [],
+    isLoading: true
   }
   groupProps = {
     appear: false,
@@ -40,38 +42,42 @@ class Developers extends Component {
       if (developers.length > chunk) {
         const stateDevelopers = developers.slice(0, chunk);
         const paginatedDevelopers = developers.slice(chunk, chunk + developers.length);
-        this.setState({ developers: stateDevelopers, paginatedDevelopers: paginatedDevelopers })
+        this.setState({ developers: stateDevelopers, paginatedDevelopers: paginatedDevelopers, isLoading: false })
       }
       else {
         this.setState({ developers })
       }
+    }).catch(err => {
+      console.log(err);
+    }).finally(() => {
+      this.setState({ isLoading: false })
     })
   }
   render() {
-    const { developers, paginatedDevelopers } = this.state
+    const { developers, paginatedDevelopers, isLoading } = this.state
     return (
       <section className="platform-wrapper section-padding">
         <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <h1 className="text-white text-capitalize pb-5">{this.state.pathName}</h1>
-            </div>
-            {/* <div className="row"> */}
-            {/* <TransitionGroup {...this.groupProps}> */}
-            {developers.map(developer => (
-              <div className="col-md-3 mb-4" key={developer.id}>
-                <Cards data={developer} />
+          {isLoading ? <Preloader /> :
+            <Fragment>
+              <div className="row">
+                <div className="col-md-12">
+                  <h1 className="text-white text-capitalize pb-5">{this.state.pathName}</h1>
+                </div>
+                {developers.map(developer => (
+                  <div className="col-md-3 mb-4" key={developer.id}>
+                    <Cards data={developer} />
+                  </div>
+                ))}
               </div>
-            ))}
-            {/* </TransitionGroup> */}
-            {/* </div> */}
-          </div>
-          {paginatedDevelopers.length > 0 &&
-            <div className="row">
-              <div className="col-md-4 mx-auto mt-3">
-                <LoadMoreBtn btnText={'Load More'} loadMore={this.loadMore} />
-              </div>
-            </div>
+              {paginatedDevelopers.length > 0 &&
+                <div className="row">
+                  <div className="col-md-4 mx-auto mt-3">
+                    <LoadMoreBtn btnText={'Load More'} loadMore={this.loadMore} />
+                  </div>
+                </div>
+              }
+            </Fragment>
           }
         </div>
       </section>
